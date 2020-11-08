82c4d86f4377c3761bbec193e4af843bd38c3398
Tavin Ho <chingching6@gmail.com>
2020-11-08 11:16:09 +0800

    fix jsx to track

---
+++ jsx/src/index.js
@@ -0,0 +1,29 @@
+// Import React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// function getButtonText() {
+//     return 'Click on me!';
+// }
+
+// Create a React component
+const App = () => {
+    const buttonText = 'Click me!';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {/* {getButtonText()} */}
+                {buttonText}
+            </button>
+        </div>
+    );
+};
+
+// Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
